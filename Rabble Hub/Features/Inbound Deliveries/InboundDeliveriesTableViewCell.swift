071cde9b53e646f99245a0b73f72d20ed27a7c5b
aantiola <aljon.antiola@cbsnorthstar.com>
2024-04-19 16:14:02 +0800

    WIP: Inbound deliveries
    WIP: Inbound deliveries

---
+++ Rabble Hub/Features/Inbound Deliveries/InboundDeliveriesTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  InboundDeliveriesTableViewCell.swift
+//  Rabble Hub
+//
+//  Created by aljon antiola on 4/18/24.
+//
+
+import UIKit
+
+class InboundDeliveriesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
