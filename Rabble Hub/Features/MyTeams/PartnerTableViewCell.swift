71ce6f0f96c0b2492edbcdac696f7dc6a27f773f
franzhenrideguzman <franzhenri.deguzman@gmail.com>
2024-04-20 13:27:02 +0800

    WIP

---
+++ Rabble Hub/Features/MyTeams/PartnerTableViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  PartnerTableViewCell.swift
+//  Rabble Hub
+//
+//  Created by Franz Henri De Guzman on 4/19/24.
+//
+
+import UIKit
+
+class PartnerTableViewCell: UITableViewCell {
+    
+    @IBOutlet var container: UIView!
+    @IBOutlet var img: UIImageView!
+    @IBOutlet var hostContainer: UIView!
+    @IBOutlet var hostLabel: UILabel!
+    @IBOutlet var membersCount: UILabel!
+    @IBOutlet var partnerCategory: UILabel!
+    @IBOutlet var deliveryDetailsLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        container.layer.borderWidth = 1.0
+        container.layer.borderColor = Colors.Gray5.cgColor
+        container.layer.cornerRadius = 8.0
+        container.clipsToBounds = true
+        
+        hostContainer.layer.cornerRadius = 12.5
+        hostContainer.clipsToBounds = true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Rabble Hub/Features/MyTeams/PartnersViewController.swift
@@ -0,0 +1,34 @@
+//
+//  PartnersViewController.swift
+//  Rabble Hub
+//
+//  Created by Franz Henri De Guzman on 4/19/24.
+//
+
+import UIKit
+
+class PartnersViewController: UIViewController {
+
+    @IBOutlet var searchBar: UISearchBar!
+    @IBOutlet var partnerTableview: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
