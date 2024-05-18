73707490a73b7276d08180f01c984066567c1742
franzhenrideguzman <franzhenri.deguzman@gmail.com>
2024-05-18 19:57:15 +0800

    feat: save store profile

---
+++ Rabble Hub/Data/ResponseModel/SaveStoreProfileResponse.swift
@@ -0,0 +1,31 @@
+//
+//  SaveStoreProfileResponse.swift
+//  Rabble Hub
+//
+//  Created by Franz Henri De Guzman on 5/18/24.
+//
+
+import Foundation
+
+struct SaveStoreProfileResponse: Codable {
+    let statusCode: Int
+    let message: String
+    let data: Store
+}
+
+struct Store: Codable {
+    let id: String
+    let userId: String
+    let name: String
+    let postalCode: String
+    let stripeConnectId: String?
+    let city: String
+    let streetAddress: String
+    let direction: String
+    let storeType: String
+    let shelfSpace: String
+    let dryStorageSpace: String
+    let createdAt: String
+    let updatedAt: String
+}
+
--- Rabble Hub/Features/SignUp/SignUpViewController.swift
+++ Rabble Hub/Features/SignUp/SignUpViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class SignUpViewController: UIViewController {
     
+    @IBOutlet var storeName: RabbleTextField!
+    @IBOutlet var postalCode: RabbleTextField!
+    @IBOutlet var city: RabbleTextField!
+    @IBOutlet var street: RabbleTextField!
+    @IBOutlet var direction: RabbleTextView!
+    @IBOutlet var storeType: RabbleTextField!
+    @IBOutlet var shelfSpace: RabbleTextField!
+    @IBOutlet var dryStorageSpace: RabbleTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,11 +31,43 @@ class SignUpViewController: UIViewController {
         }
     }
     
+    func saveStoreProfile(){
+        apiprovider.request(.saveStoreProfile(name: storeName.text ?? "", postalCode: postalCode.text ?? "", city: city.text ?? "", streetAddress: street.text ?? "", direction: direction.text ?? "", storeType: storeType.text ?? "", shelfSpace: shelfSpace.text ?? "", dryStorageSpace: dryStorageSpace.text ?? "", baseURL: environment.baseURL)) { result in
+            switch result {
+            case let .success(response):
+                // Handle successful response
+                do {
+                    let response = try response.map(SaveStoreProfileResponse.self)
+                    if response.statusCode == 200 || response.statusCode == 201 {
+                        print(response.data)
+                        self.saveStore(response.data)
+                        self.goToSavePartnerProfile()
+                    }else{
+                        print("Error Message: \(response.message)")
+                    }
+                    
+                } catch {
+                    print("Failed to map response data: \(error)")
+                }
+            case let .failure(error):
+                // Handle error
+                print("Request failed with error: \(error)")
+            }
+        }
+    }
+    
+    func saveStore(_ store: Store) {
+        StoreManager.shared.store = store
+    }
+    
     @IBAction func nextButtonTap(_ sender: Any) {
+        self.saveStoreProfile()
+    }
+    
+    func goToSavePartnerProfile(){
         let signUpView = UIStoryboard(name: "SignUpView", bundle: nil)
         let vc = signUpView.instantiateViewController(withIdentifier: "SignUpProfileViewController") as! SignUpProfileViewController
         vc.modalPresentationStyle = .fullScreen
         present(vc, animated: false, completion: nil)
     }
-    
 }
