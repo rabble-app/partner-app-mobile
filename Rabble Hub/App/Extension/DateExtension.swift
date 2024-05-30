99022481ce69184ad5f40cfbfb58d2ec800bf974
aantiola <antiolaaljon@gmail.com>
2024-05-30 23:06:23 +0800

    Calendar UI changes and fixes.

--- Rabble Hub/App/Extension/DateExtension.swift
+++ Rabble Hub/App/Extension/DateExtension.swift
@@ -85,6 +85,63 @@ extension Date {
     }
     
     
+    /// Determines if there are two weeks or more remaining from the current date to the end of the month.
+    /// - Returns: `true` if there are 14 or more days remaining in the month, including today; otherwise, `false`.
+    func isTwoWeeksOrMoreToMonthEnd() -> Bool {
+        let calendar = Calendar.current
+        
+        // Get the range of days in the current month
+        guard let range = calendar.range(of: .day, in: .month, for: self) else {
+            return false
+        }
+        
+        // Get the number of days in the current month
+        let numDaysInMonth = range.count
+        
+        // Get the current day of the month
+        let currentDay = calendar.component(.day, from: self)
+        
+        // Calculate the number of days remaining in the month, including today
+        let daysRemaining = numDaysInMonth - currentDay + 1
+        
+        return daysRemaining >= 14
+    }
+    
+    
+    /// Returns the date range starting from the 2nd week from the current date up to the 10th week.
+    /// - Returns: A tuple containing the start date and end date of the range.
+    func dateRangeFrom2ndTo10thWeek() -> (startDate: Date, endDate: Date) {
+        let calendar = Calendar.current
+        
+        // Calculate the start date, which is 2 weeks from today
+        guard let startDate = calendar.date(byAdding: .weekOfYear, value: 2, to: self) else {
+            fatalError("Failed to calculate the start date")
+        }
+        
+        // Calculate the end date, which is 10 weeks from today
+        guard let endDate = calendar.date(byAdding: .weekOfYear, value: 10, to: self) else {
+            fatalError("Failed to calculate the end date")
+        }
+        
+        return (startDate, endDate)
+    }
+    
+    
+    /// Checks if the given date is outside the range starting from the 2nd week to the 10th week from the current date.
+    /// - Parameter date: The date to check.
+    /// - Returns: `true` if the given date is outside the range; otherwise, `false`.
+    func isDateOutside2ndTo10thWeekRange(date: Date) -> Bool {
+        let range = date.dateRangeFrom2ndTo10thWeek()
+        
+        // Check if the given date is before the start date or after the end date of the range
+        if self < range.startDate || self > range.endDate {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    
     /// Converts the `Date` instance to a string in the format "yyyy-MM-dd HH:mm:ss.SSS".
     ///
     /// - Returns: A string representation of the `Date` instance.
