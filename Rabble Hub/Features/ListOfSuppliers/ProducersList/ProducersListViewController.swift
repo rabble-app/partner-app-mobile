5484019adca7b557300c91da0be59783df1de09d
franzhenrideguzman <franzhenri.deguzman@gmail.com>
2024-06-03 13:23:57 +0800

    Bugfix: Removed toast message in supplier list

--- Rabble Hub/Features/ListOfSuppliers/ProducersList/ProducersListViewController.swift
+++ Rabble Hub/Features/ListOfSuppliers/ProducersList/ProducersListViewController.swift
@@ -63,7 +63,7 @@ class ProducersListViewController: UIViewController {
             let suppliersResponse = try response.map(GetSuppliersResponse.self)
             if suppliersResponse.statusCode == 200 {
                 self.updateSuppliers(suppliersResponse.data ?? [])
-                SnackBar().alert(withMessage: suppliersResponse.message, isSuccess: true, parent: self.view)
+//                SnackBar().alert(withMessage: suppliersResponse.message, isSuccess: true, parent: self.view)
             } else {
                 self.showError(suppliersResponse.message)
             }
