dfe2f0fa33970a0c30218e8187dbda98626a8909
franzhenrideguzman <franzhenri.deguzman@gmail.com>
2024-06-16 00:38:48 +0800

    optimised signupagreement

--- Rabble Hub/Features/SignUp/SignUpAgreementViewController.swift
+++ Rabble Hub/Features/SignUp/SignUpAgreementViewController.swift
@@ -21,19 +21,23 @@ class SignUpAgreementViewController: UIViewController, PaymentPopUpDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpView()
-        previousStepButton.isEnabled = !isFromOnboardingStage
+        configurePreviousStepButton()
     }
     
     private func setUpView() {
-        firstAgreementLabel.attributedText = configureString(
+        firstAgreementLabel.attributedText = createAttributedString(
             fullString: "Commission Rabble agree to pay a standard commission on every order completed at your location, subject to Rabble’s Partnership Agreement.",
             boldPartOfString: "Commission"
         )
         
-        secondAgreementLabel.attributedText = configureString(
+        secondAgreementLabel.attributedText = createAttributedString(
             fullString: "Each order will be stored securely in the store. You or an employee is to confirm the customer booking.",
             boldPartOfString: "Each order will be stored securely in the store."
         )
+    }
+    
+    private func configurePreviousStepButton() {
+        previousStepButton.isEnabled = !isFromOnboardingStage
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -70,7 +74,7 @@ class SignUpAgreementViewController: UIViewController, PaymentPopUpDelegate {
             let userResponse = try response.map(UpdateUserRecordResponse.self)
             if userResponse.statusCode == 200 || userResponse.statusCode == 201, let user = userResponse.data {
                 updateUserData(with: user)
-                gotoMainTab()
+                navigateToMainTab()
             } else {
                 showSnackBar(message: userResponse.message)
             }
@@ -95,19 +99,18 @@ class SignUpAgreementViewController: UIViewController, PaymentPopUpDelegate {
     }
     
     private func updateUserData(with userRecord: UserRecord) {
-        if var userData = userDataManager.getUserData() {
-            userData.phone = userRecord.phone
-            userData.email = userRecord.email
-            userData.firstName = userRecord.firstName
-            userData.lastName = userRecord.lastName
-            userData.partner?.postalCode = userRecord.postalCode
-            userData.stripeCustomerId = userRecord.stripeCustomerId
-            userData.onboardingStage = userRecord.onboardingStage ?? 4
-            userDataManager.saveUserData(userData)
-        }
+        guard var userData = userDataManager.getUserData() else { return }
+        userData.phone = userRecord.phone
+        userData.email = userRecord.email
+        userData.firstName = userRecord.firstName
+        userData.lastName = userRecord.lastName
+        userData.partner?.postalCode = userRecord.postalCode
+        userData.stripeCustomerId = userRecord.stripeCustomerId
+        userData.onboardingStage = userRecord.onboardingStage ?? 4
+        userDataManager.saveUserData(userData)
     }
     
-    private func gotoMainTab() {
+    private func navigateToMainTab() {
         let storyboard = UIStoryboard(name: "PaymentPopView", bundle: .main)
         if let vc = storyboard.instantiateViewController(withIdentifier: "PaymentPopUpViewController") as? PaymentPopUpViewController {
             vc.modalPresentationStyle = .overFullScreen
@@ -116,15 +119,18 @@ class SignUpAgreementViewController: UIViewController, PaymentPopUpDelegate {
         }
     }
     
-    private func configureString(fullString: NSString, boldPartOfString: NSString) -> NSAttributedString? {
+    private func createAttributedString(fullString: String, boldPartOfString: String) -> NSAttributedString? {
         guard let font = UIFont(name: Properties.Font.SF_PRO_REGULAR, size: 16),
               let boldFont = UIFont(name: Properties.Font.SF_PRO_SEMIBOLD, size: 16) else { return nil }
         
-        let nonBoldAttributes = [NSAttributedString.Key.font: font, NSAttributedString.Key.foregroundColor: Colors.Gray3]
-        let boldAttributes = [NSAttributedString.Key.font: boldFont, NSAttributedString.Key.foregroundColor: Colors.Gray1]
+        let nonBoldAttributes: [NSAttributedString.Key: Any] = [.font: font, .foregroundColor: Colors.Gray3]
+        let boldAttributes: [NSAttributedString.Key: Any] = [.font: boldFont, .foregroundColor: Colors.Gray1]
         
-        let attributedString = NSMutableAttributedString(string: fullString as String, attributes: nonBoldAttributes)
-        attributedString.addAttributes(boldAttributes, range: fullString.range(of: boldPartOfString as String))
+        let attributedString = NSMutableAttributedString(string: fullString, attributes: nonBoldAttributes)
+        if let range = fullString.range(of: boldPartOfString) {
+            let nsRange = NSRange(range, in: fullString)
+            attributedString.addAttributes(boldAttributes, range: nsRange)
+        }
         
         return attributedString
     }
