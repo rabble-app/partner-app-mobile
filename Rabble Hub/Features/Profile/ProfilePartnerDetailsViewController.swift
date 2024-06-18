ba77260ba6534855b880ac6996923ec6c478467c
aantiola <antiolaaljon@gmail.com>
2024-06-18 19:45:13 +0800

    Implement Store Information API on Partner Details view.

--- Rabble Hub/Features/Profile/ProfilePartnerDetailsViewController.swift
+++ Rabble Hub/Features/Profile/ProfilePartnerDetailsViewController.swift
@@ -5,10 +5,16 @@
 //  Created by aljon antiola on 4/25/24.
 //
 
+import Moya
 import UIKit
 
 class ProfilePartnerDetailsViewController: UIViewController {
 
+    @IBOutlet weak var storeNameTextField: RabbleTextField!
+    @IBOutlet weak var postalCodeTextField: RabbleTextField!
+    @IBOutlet weak var cityTextField: RabbleTextField!
+    @IBOutlet weak var streetAddressTextField: RabbleTextField!
+    @IBOutlet weak var directionsTextView: RabbleTextView!
     @IBOutlet weak var storeTypeTextfield: RabbleTextField!
     @IBOutlet weak var fridgeSpaceTextField: RabbleTextField!
     @IBOutlet weak var dryStorageTextField: RabbleTextField!
@@ -19,9 +25,17 @@ class ProfilePartnerDetailsViewController: UIViewController {
     
     @IBOutlet weak var popupBackgroundView: UIView!
     
+    var apiProvider: MoyaProvider<RabbleHubAPI> = APIProvider
+    private let userDataManager = UserDataManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        fetchPartnerDetails()
     }
     
     func setupUI() {
@@ -30,6 +44,70 @@ class ProfilePartnerDetailsViewController: UIViewController {
         self.dryStorageButton.setTitle("", for: .normal)
     }
    
+    private func fetchPartnerDetails() {
+        guard let partner = userDataManager.getUserData()?.partner else { return }
+        apiProvider.request(.getStoreInformation(partnerId: partner.id)) { result in
+            self.handleSuppliersResponse(result)
+        }
+    }
+    
+    private func handleSuppliersResponse(_ result: Result<Response, MoyaError>) {
+        switch result {
+        case .success(let response):
+            handleSuccessResponse(response)
+        case .failure(let error):
+            displaySnackBar(message: (error.localizedDescription), isSuccess: false)
+        }
+    }
+    
+    private func handleSuccessResponse(_ response: Response) {
+        do {
+            let storeInformationResponse = try response.map(StoreInformationResponse.self)
+            if storeInformationResponse.statusCode == 200 {
+                self.configureStoreInformationUI(storeData: storeInformationResponse.data)
+            } else {
+                displaySnackBar(message: storeInformationResponse.message, isSuccess: true)
+            }
+        } catch {
+            handleMappingError(response)
+        }
+    }
+    
+    private func handleMappingError(_ response: Response) {
+        do {
+            let errorResponse = try response.map(StandardResponse.self)
+            displaySnackBar(message: errorResponse.message, isSuccess: false)
+        } catch {
+            print("Failed to map response data: \(error)")
+        }
+    }
+    
+
+    /// Configure Store Information fields
+    func configureStoreInformationUI(storeData: StoreData) {
+        self.storeNameTextField.text = storeData.name
+        self.postalCodeTextField.text = storeData.postalCode
+ 
+        if let city = storeData.city {
+            self.cityTextField.text = city
+        }
+        if let streetAddress = storeData.streetAddress {
+            self.streetAddressTextField.text = streetAddress
+        }
+        if let direction = storeData.direction {
+            self.directionsTextView.text = direction
+        }
+        if let storeType = storeData.storeType {
+            self.storeTypeTextfield.text = storeType
+        }
+        if let shelfSpace = storeData.shelfSpace {
+            self.fridgeSpaceTextField.text = shelfSpace
+        }
+        if let dryStorageSpace = storeData.dryStorageSpace {
+            self.dryStorageTextField.text = dryStorageSpace
+        }
+        
+    }
     
     @IBAction func storeTypeButtonTapped(_ sender: Any) {
         self.showViewWithAnimation(view: self.popupBackgroundView)
@@ -105,5 +183,9 @@ extension ProfilePartnerDetailsViewController {
         UIView.animate(withDuration: duration) {
             view.alpha = 1.0
         }
+    }
+    
+    private func displaySnackBar(message: String, isSuccess: Bool = false) {
+        SnackBar().alert(withMessage: message, isSuccess: isSuccess, parent: view)
     }
 }
