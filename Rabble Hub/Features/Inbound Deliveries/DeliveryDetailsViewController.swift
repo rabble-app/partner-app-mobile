abdc1631f7f263aa49e756ef7e7c6ab1a752507f
franzhenrideguzman <franzhenri.deguzman@gmail.com>
2024-07-15 22:27:39 +0800

    The order details shows 4 products whereas it has 5 and you can also see when I manually confirm my order it has 5 products.

--- Rabble Hub/Features/Inbound Deliveries/DeliveryDetailsViewController.swift
+++ Rabble Hub/Features/Inbound Deliveries/DeliveryDetailsViewController.swift
@@ -112,7 +112,7 @@ class DeliveryDetailsViewController: UIViewController {
     
     private func updateInboundDeliveryDetails(_ orderDetailsResponse: [OrderDetail]) {
         orderDetails = orderDetailsResponse
-        tableViewConstraintHeight.constant = CGFloat(77 * orderDetails.count) + 20
+        tableViewConstraintHeight.constant = CGFloat(77 * orderDetails.count) + 10 + 77
         tableView.isHidden = orderDetails.isEmpty
         tableView.reloadData()
     }
