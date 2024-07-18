e940e96ce56af79887ebf5bf831a97b81b5c35b5
franzhenrideguzman <franzhenri.deguzman@gmail.com>
2024-07-18 16:32:41 +0800

    Added open link in stripe connect

--- Rabble Hub/Features/GettingStarted/PaymentPopUp/PaymentPopUpViewController.swift
+++ Rabble Hub/Features/GettingStarted/PaymentPopUp/PaymentPopUpViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SafariServices
 
 protocol PaymentPopUpDelegate: AnyObject {
     func paymentPopUpDismissed()
@@ -26,8 +27,15 @@ class PaymentPopUpViewController: UIViewController, ShowSuppliersDelegate {
     }
     
     @IBAction func connectButtonTap(_ sender: Any) {
-      
+        let userDataManager = UserDataManager()
+        let id = userDataManager.getUserData()?.partner?.id
+        guard let url = URL(string: "https://supplier.rabble.market/auth/stripe/onboard-user?partnerId=\(id ?? "")") else { return }
+        let safariViewController = SFSafariViewController(url: url)
+        safariViewController.modalPresentationStyle = .overFullScreen
+        present(safariViewController, animated: true, completion: nil)
     }
+    
+    
     
     @IBAction func skipButtonTap(_ sender: Any) {
         
