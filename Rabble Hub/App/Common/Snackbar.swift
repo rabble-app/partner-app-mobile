20e0f054612cbf170430bc99b80b2e713975987e
aantiola <antiolaaljon@gmail.com>
2024-07-20 21:45:38 +0800

    Refactor SnackBar. Fixed colors

--- Rabble Hub/App/Common/Snackbar.swift
+++ Rabble Hub/App/Common/Snackbar.swift
@@ -14,28 +14,14 @@ class SnackBar {
     var tparent: UIView?
     
     func alert(withMessage msg: String, isSuccess: Bool, parent: UIView) {
-        guard let topWindow = UIApplication.shared.windows.first(where: { $0.isKeyWindow }) else {
-            return
-        }
-        
-        tparent = topWindow
-        
-        let topPadding: CGFloat = tparent?.safeAreaInsets.top ?? 0
-        let horizontalPadding: CGFloat = 16
-        let iconSize = CGSize(width: 20, height: 19)
-        
-        let iconImageView = createIconImageView(isSuccess: isSuccess, iconSize: iconSize)
-        let messageLabel = createMessageLabel(message: msg, availableWidth: parent.bounds.size.width - (horizontalPadding * 2) - iconSize.width, horizontalPadding: horizontalPadding)
-        
-        let snackbarHeight = max(messageLabel.bounds.height + 32, 54)
-        
-        let initialFrame = CGRect(x: horizontalPadding / 2, y: -snackbarHeight, width: parent.bounds.size.width - horizontalPadding, height: snackbarHeight)
-        let finalFrame = CGRect(x: horizontalPadding / 2, y: topPadding + horizontalPadding / 2, width: parent.bounds.size.width - horizontalPadding, height: snackbarHeight)
-        
-        setupSnackbarView(initialFrame: initialFrame, finalFrame: finalFrame, backgroundColor: isSuccess ? Colors.ToastSuccessBackgroundColor : Colors.ToastErrorBackgroundColor, iconImageView: iconImageView, messageLabel: messageLabel, duration: isSuccess ? 2.0 : 3.0)
+        showAlert(withMessage: msg, iconName: isSuccess ? "toast_success" : "toast_error", backgroundColor: isSuccess ? Colors.ToastSuccessBackgroundColor : Colors.ToastErrorBackgroundColor, parent: parent, duration: isSuccess ? 2.0 : 3.0, textColor: isSuccess ? Colors.ToastSuccessFontColor : Colors.ToastErrorFontColor)
     }
     
     func alertInfo(withMessage msg: String, parent: UIView) {
+        showAlert(withMessage: msg, iconName: "toast_info", backgroundColor: Colors.ToastInfoBackgroundColor, parent: parent, duration: 3.0, textColor: Colors.ToastInfoFontColor)
+    }
+    
+    private func showAlert(withMessage msg: String, iconName: String, backgroundColor: UIColor, parent: UIView, duration: Double, textColor: UIColor) {
         guard let topWindow = UIApplication.shared.windows.first(where: { $0.isKeyWindow }) else {
             return
         }
@@ -46,53 +32,29 @@ class SnackBar {
         let horizontalPadding: CGFloat = 16
         let iconSize = CGSize(width: 20, height: 19)
         
-        let iconImageView = createIconImageViewInfo(iconSize: iconSize)
-        let messageLabel = createMessageLabelInfo(message: msg, availableWidth: parent.bounds.size.width - (horizontalPadding * 2) - iconSize.width, horizontalPadding: horizontalPadding)
+        let iconImageView = createIconImageView(iconName: iconName, iconSize: iconSize)
+        let messageLabel = createMessageLabel(message: msg, availableWidth: parent.bounds.size.width - (horizontalPadding * 2) - iconSize.width, horizontalPadding: horizontalPadding, textColor: textColor)
         
         let snackbarHeight = max(messageLabel.bounds.height + 32, 54)
         
         let initialFrame = CGRect(x: horizontalPadding / 2, y: -snackbarHeight, width: parent.bounds.size.width - horizontalPadding, height: snackbarHeight)
         let finalFrame = CGRect(x: horizontalPadding / 2, y: topPadding + horizontalPadding / 2, width: parent.bounds.size.width - horizontalPadding, height: snackbarHeight)
         
-        setupSnackbarView(initialFrame: initialFrame, finalFrame: finalFrame, backgroundColor: Colors.ToastInfoBackgroundColor, iconImageView: iconImageView, messageLabel: messageLabel, duration: 3.0)
+        setupSnackbarView(initialFrame: initialFrame, finalFrame: finalFrame, backgroundColor: backgroundColor, iconImageView: iconImageView, messageLabel: messageLabel, duration: duration)
     }
     
-    private func createIconImageView(isSuccess: Bool, iconSize: CGSize) -> UIImageView {
+    private func createIconImageView(iconName: String, iconSize: CGSize) -> UIImageView {
         let iconImageView = UIImageView(frame: CGRect(x: 12, y: 17, width: iconSize.width, height: iconSize.height))
-        if isSuccess {
-            iconImageView.image = UIImage(named: "toast_success")
-        } else {
-            iconImageView.image = UIImage(named: "toast_error")
-        }
+        iconImageView.image = UIImage(named: iconName)
         return iconImageView
     }
     
-    private func createIconImageViewInfo(iconSize: CGSize) -> UIImageView {
-        let iconImageView = UIImageView(frame: CGRect(x: 12, y: 17, width: iconSize.width, height: iconSize.height))
-        iconImageView.image = UIImage(named: "toast_info")
-        return iconImageView
-    }
-    
-    private func createMessageLabelInfo(message: String, availableWidth: CGFloat, horizontalPadding: CGFloat) -> UILabel {
+    private func createMessageLabel(message: String, availableWidth: CGFloat, horizontalPadding: CGFloat, textColor: UIColor) -> UILabel {
         let label = UILabel()
         label.numberOfLines = 0
         label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
         label.text = message
-        label.textColor = Colors.ToastInfoBackgroundColor
-        
-        let labelSize = message.boundingRect(with: CGSize(width: availableWidth, height: .greatestFiniteMagnitude), options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: label.font!], context: nil).size
-        
-        label.frame = CGRect(x: 24 + horizontalPadding, y: 18, width: availableWidth, height: labelSize.height)
-        
-        return label
-    }
-    
-    private func createMessageLabel(message: String, availableWidth: CGFloat, horizontalPadding: CGFloat) -> UILabel {
-        let label = UILabel()
-        label.numberOfLines = 0
-        label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
-        label.text = message
-        label.textColor = Colors.ToastErrorFontColor
+        label.textColor = textColor
         
         let labelSize = message.boundingRect(with: CGSize(width: availableWidth, height: .greatestFiniteMagnitude), options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: label.font!], context: nil).size
         
