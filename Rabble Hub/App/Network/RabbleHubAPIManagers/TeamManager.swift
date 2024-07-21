c08a549adfd57265e2b333afab944e788a6a70c8
aantiola <antiolaaljon@gmail.com>
2024-07-21 17:12:33 +0800

    Updated comments

--- Rabble Hub/App/Network/RabbleHubAPIManagers/TeamManager.swift
+++ Rabble Hub/App/Network/RabbleHubAPIManagers/TeamManager.swift
@@ -12,11 +12,18 @@ import Moya
 class TeamManager {
     private var apiProvider: MoyaProvider<RabbleHubAPI> = APIProvider
     
-    /// Fetches partner teams based on user data.
-    ///
-    /// - Parameters:
-    ///   - userDataManager: The user data manager instance to retrieve user data.
-    ///   - completion: A completion handler with a result containing either the fetched partner teams response or an error.
+    /**
+     Fetches partner teams for a user.
+     
+     - Parameters:
+     - userDataManager: An instance of `UserDataManager` used to retrieve user data.
+     - completion: A closure to be executed once the request has finished. The closure takes a `Result` containing either a `GetPartnerTeamsResponse` object or an `Error`.
+     
+     The function first checks if the user data is valid. If not, it calls the completion handler with an error. If the user is an employee, it uses the employee's partner user ID. Otherwise, it uses the user ID directly to make a network request to fetch the partner teams and handles the response.
+     
+     - Note:
+     If the user data or the relevant user ID is invalid, the function will immediately call the completion handler with an error indicating an invalid user.
+     */
     func fetchPartnerTeams(userDataManager: UserDataManager, completion: @escaping (Result<GetPartnerTeamsResponse, Error>) -> Void) {
         guard let userData = userDataManager.getUserData() else {
             completion(.failure(NSError(domain: "Invalid user data", code: -1, userInfo: nil)))
@@ -39,6 +46,18 @@ class TeamManager {
         }
     }
     
+    /**
+     Fetches inbound delivery details for a given team ID.
+     
+     - Parameters:
+     - teamId: An optional `String` representing the team ID.
+     - completion: A closure to be executed once the request has finished. The closure takes a `Result` containing either an `OrderDetailsResponse` object or an `Error`.
+     
+     The function first checks if the `teamId` is valid. If not, it calls the completion handler with an error. Otherwise, it makes a network request to fetch the delivery details using the provided team ID and handles the response.
+     
+     - Note:
+     If `teamId` is nil, the function will immediately call the completion handler with an error indicating an invalid team ID.
+     */
     func getInboundDeliveryDetails(teamId: String?, completion: @escaping (Result<OrderDetailsResponse, Error>) -> Void) {
         
         guard let id = teamId else {
@@ -50,11 +69,22 @@ class TeamManager {
             self.handleResponse(result, completion: completion)
         }
     }
-    /// Handles the API response and decodes the data.
-    ///
-    /// - Parameters:
-    ///   - result: The result of the API request containing either a response or an error.
-    ///   - completion: A completion handler with a result containing either the decoded response or an error.
+    
+}
+
+extension TeamManager {
+    /**
+     Handles the response of a network request.
+     
+     - Parameters:
+     - result: A `Result` object containing either a `Response` or a `MoyaError`.
+     - completion: A closure to be executed once the response has been handled. The closure takes a `Result` containing either a decoded object of type `T` or an `Error`.
+     
+     The function decodes the response into the expected type `T`. If decoding fails, it attempts to decode an error message and passes it to the completion handler. If any errors occur during these processes, it passes the error to the completion handler.
+     
+     - Note:
+     The function assumes that the response can be decoded into the expected type `T` or a `StandardResponse` containing an error message.
+     */
     private func handleResponse<T: Decodable>(_ result: Result<Response, MoyaError>, completion: @escaping (Result<T, Error>) -> Void) {
         switch result {
         case .success(let response):
