6135855193d689f98f595ef0e5ef9c8ccb0552d4
aantiola <antiolaaljon@gmail.com>
2024-07-23 07:52:56 +0800

    WIP: Order confirmation description

--- Rabble Hub/Features/Inbound Deliveries/DeliveryDetailsViewController.swift
+++ Rabble Hub/Features/Inbound Deliveries/DeliveryDetailsViewController.swift
@@ -188,14 +188,17 @@ class DeliveryDetailsViewController: UIViewController {
             view.layoutIfNeeded()
         }
 
-        if !detail.team.description.isEmpty {
-            descriptionTextView.text = detail.team.description
-            adjustTextViewHeight()
-        } else {
-            descriptionTextContainerView.isHidden = true
-            descriptionTextViewHeightConstraint.constant = .zero
-            view.layoutIfNeeded()
-        }
+//        if !detail.team.description.isEmpty {
+//            descriptionTextView.text = detail.team.description
+//            adjustTextViewHeight()
+//        } else {
+//            descriptionTextContainerView.isHidden = true
+//            descriptionTextViewHeightConstraint.constant = .zero
+//            view.layoutIfNeeded()
+//        }
+        // WIP: Needs response from Order Confirmation
+        descriptionTextContainerView.isHidden = true
+        descriptionTextViewHeightConstraint.constant = .zero
         
         tableView.reloadData()
     }
